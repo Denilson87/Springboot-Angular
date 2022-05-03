b145f0c1d00b4ab5027adca828b7f122b8c2aabd
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-03 05:29:35 -0700

    building the serverService implementations

---
+++ src/main/java/com/app/servers/service/serverImplementation/ServerServiceImplementation.java
@@ -0,0 +1,34 @@
+package com.app.servers.service.serverImplementation;
+
+import com.app.servers.model.Server;
+import com.app.servers.service.ServerService;
+
+import java.util.Collection;
+
+public class ServerServiceImplementation implements ServerService {
+
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
