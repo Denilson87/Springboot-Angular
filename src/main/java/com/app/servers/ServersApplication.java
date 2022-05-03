0f2f547078633f54d50ea34d05e7311e59e29967
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-03 04:53:14 -0700

    updating the model or domain

--- src/main/java/com/app/servers/ServersApplication.java
+++ src/main/java/com/app/servers/ServersApplication.java
@@ -2,12 +2,20 @@ package com.app.servers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class ServersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServersApplication.class, args);
+	}
+	@GetMapping("/api/hello")
+	public String index(){
+		return "Hellow world";
 	}
 
 }
---
+++ src/main/java/com/app/servers/model/Server.java
@@ -0,0 +1,29 @@
+package com.app.servers.model;
+
+import com.app.servers.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP address can not be empty")
+    private String ipAdress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
