845bc287bfa5093a88d8db5ca456299d36826096
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-03 16:02:15 -0700

    ping method is now ready

--- src/main/java/com/app/servers/controllers/ServerController.java
+++ src/main/java/com/app/servers/controllers/ServerController.java
@@ -1,6 +1,9 @@
 package com.app.servers.controllers;
 
+import com.app.servers.Status;
 import com.app.servers.model.Response;
+import com.app.servers.model.Server;
+import com.app.servers.service.ServerService;
 import com.app.servers.service.serverImplementation.ServerServiceImplementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static com.app.servers.Status.SERVER_UP;
 import static java.time.LocalDateTime.now;
 import static java.util.Map.*;
 import static org.springframework.http.HttpStatus.OK;
@@ -36,11 +41,13 @@ public class ServerController {
     }
 
     @GetMapping("/ping{ipAddress}")
-    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress")String ipAddress){
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress")String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
-                        .data(of("servers",serverService.ping(30)))
+                        .data(of("server",server))
+                        .message(server.getStatus() == SERVER_UP ? "Server is up" : "Ping failure")
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
