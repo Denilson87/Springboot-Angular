a1d833ba0f9052567f5e59c132435f809f5003bb
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-03 14:01:29 -0700

    finishing the controller list

--- src/main/java/com/app/servers/controllers/ServerController.java
+++ src/main/java/com/app/servers/controllers/ServerController.java
@@ -4,6 +4,7 @@ import com.app.servers.model.Response;
 import com.app.servers.service.serverImplementation.ServerServiceImplementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +12,22 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
+import static java.util.Map.*;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("server/api/")
+@RequestMapping("/server")
 
 public class ServerController {
     private final ServerServiceImplementation serverService;
 
+    @GetMapping("/list")
     public ResponseEntity<Response> getServer(){
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
-                        .data(Map.of("servers",serverService.list(30)))
+                        .data(of("servers",serverService.list(30)))
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
