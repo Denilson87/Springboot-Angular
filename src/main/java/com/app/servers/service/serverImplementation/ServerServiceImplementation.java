5ff3d5a538971b9d3fd8d7205c1ea2245dec6fc9
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-03 05:54:18 -0700

    adding the log and setting imageUrl

--- src/main/java/com/app/servers/service/serverImplementation/ServerServiceImplementation.java
+++ src/main/java/com/app/servers/service/serverImplementation/ServerServiceImplementation.java
@@ -1,15 +1,26 @@
 package com.app.servers.service.serverImplementation;
 
 import com.app.servers.model.Server;
+import com.app.servers.repositories.ServerRepository;
 import com.app.servers.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
-
+@RequiredArgsConstructor
+@Transactional
+@Service
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
 
+    private final ServerRepository serverRepository;
 
     @Override
     public Server create(Server server) {
+        log.info("saving server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
         return null;
     }
 
@@ -35,6 +46,10 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
