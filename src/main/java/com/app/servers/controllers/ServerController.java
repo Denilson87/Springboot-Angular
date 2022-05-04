1549cc64fe99b7df74405e6ed2b8e80918b0ac06
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-04 01:10:02 -0700

    Building the method save

--- src/main/java/com/app/servers/controllers/ServerController.java
+++ src/main/java/com/app/servers/controllers/ServerController.java
@@ -7,11 +7,9 @@ import com.app.servers.service.ServerService;
 import com.app.servers.service.serverImplementation.ServerServiceImplementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -48,6 +46,19 @@ public class ServerController {
                         .timeStamp(now())
                         .data(of("server",server))
                         .message(server.getStatus() == SERVER_UP ? "Server is up" : "Ping failure")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("server",server))
+                        .message(server.)
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
