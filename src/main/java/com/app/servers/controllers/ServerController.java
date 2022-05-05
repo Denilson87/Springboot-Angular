5dbc37bea9bc87011a685c64ccc8176471dfca51
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-04 22:42:13 -0700

    The Backend API is ready!
    Updating the paths on URI

--- src/main/java/com/app/servers/controllers/ServerController.java
+++ src/main/java/com/app/servers/controllers/ServerController.java
@@ -44,7 +44,7 @@ public class ServerController {
         );
     }
 
-    @GetMapping("/ping{ipAddress}")
+    @GetMapping("/ping/{ipAddress}")
     public ResponseEntity<Response> pingServer(@PathVariable("ipAddress")String ipAddress) throws IOException {
         Server server = serverService.ping(ipAddress);
         return ResponseEntity.ok(
@@ -71,7 +71,7 @@ public class ServerController {
         );
     }
 
-    @GetMapping("/get{id}")
+    @GetMapping("/get/{id}")
     public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
         return ResponseEntity.ok(
                 Response.builder()
@@ -99,6 +99,6 @@ public class ServerController {
 
     @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images/" + fileName));
     }
 }
