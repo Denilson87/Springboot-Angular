512b96f1b01f9609a1fd757ee715c86020ad865c
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-05-04 17:08:21 -0700

    The backend server is Done!

--- src/main/java/com/app/servers/ServersApplication.java
+++ src/main/java/com/app/servers/ServersApplication.java
@@ -1,9 +1,15 @@
 package com.app.servers;
 
+import com.app.servers.model.Server;
+import com.app.servers.repositories.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.app.servers.Status.*;
 
 
 @SpringBootApplication
@@ -13,9 +19,12 @@ public class ServersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServersApplication.class, args);
 	}
-	@GetMapping("/api/hello")
-	public String index(){
-		return "Hellow world";
-	}
+//	@Bean
+//	CommandLineRunner run(ServerRepository serverRepository){
+//		return args ->{
+//			serverRepository.save(new Server(null,"192.168.10.01","Ubunto Server", "65 GB", "Email Server", "http://localhost:8080/image/server1.png", SERVER_UP));
+//		};
+//
+//	}
 
 }
